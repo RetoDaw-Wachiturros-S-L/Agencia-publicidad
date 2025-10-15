478cb951055ed9a83d48fe1864d9528fa562cd74
David Gonzalez <david.gonzalez.48848@iksale.egibide.org>
2025-10-15 15:24:41 +0200

    feat: started the structure of the views

---
+++ Agencia-publicidad/Views/index.php
@@ -0,0 +1,23 @@
+<?php
+    //Comprobar session
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <h1>🎨 Bienvenido a Nuestra Agencia de Publicidad</h1>
+        <p>Esta es la página principal de la agencia.</p>
+        <nav>
+        <a href='index.php?controller=OutController&accion=store'>📝 Registro de Usuario</a><br>
+        <a href='index.php?controller=MainController&accion=about'>ℹ️ Sobre Nosotros</a><br>
+        <a href='index.php?controller=OutController&accion=iniciarSesion'>Login de Usuario</a><br>
+        <a href='index.php?controller=OutController&accion=logout'>Logout</a><br>
+        </nav>
+</body>
+</html>
