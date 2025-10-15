134b759f7c42f2eaa5bed880fa33c9124d3fe3c7
David Gonzalez <david.gonzalez.48848@iksale.egibide.org>
2025-10-15 15:30:45 +0200

    fix:created DBUser Model, restructured DB functions files

---
+++ Agencia-publicidad/models/dataBase/DBUser.php
@@ -0,0 +1,43 @@
+<?php 
+//CRUD de tabla usuarios
+namespace AgenciaPublicidad\Models;
+
+require_once __DIR__ . '/DBCon.php';
+
+class DBUser {
+    
+    public function guardarUsuario(UsuarioRegistrado $usuario) {
+        $pdo = DBCon::getConnection();
+        $sql = "INSERT INTO usuarios (nombre, apellido, email, password_hash, tipo_usuario) 
+                VALUES (:nombre, :apellido, :email, :contrasenna, :tipo_usuario)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':nombre', $usuario->getNombre());
+        $stmt->bindValue(':apellido', $usuario->getApellido());
+        $stmt->bindValue(':email', $usuario->getEmail());
+        // Hashear la contraseña antes de guardarla
+        $hashedPassword = password_hash($usuario->getPassword(), PASSWORD_BCRYPT);
+        $stmt->bindValue(':contrasenna', $hashedPassword);
+        $stmt->bindValue(':tipo_usuario', $usuario->getTipo()->value);
+        
+        return $stmt->execute();
+    }
+
+    public function comprobarUsuario($email,$contrasena) {
+        $pdo = DBCon::getConnection();
+        $sql = "SELECT * FROM usuarios WHERE email = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':email', $email);
+        $stmt->execute();
+        $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+        if ($usuario && isset($usuario['password_hash'])) {
+            if (password_verify($contrasena, $usuario['password_hash'])==1){
+                return $usuario;
+            } else {
+                return "";
+            }
+        } else {
+            return "";
+        }
+    }
+}
+?>
