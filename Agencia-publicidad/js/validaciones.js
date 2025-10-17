47a78dea374ce05ae70f11b75c4ea34590de7711
DaniTo <dani.tomicic@ikasle.egibide.org>
2025-10-17 12:15:39 +0200

    feat : added some comments for future ideas + validations in js

--- Agencia-publicidad/js/validaciones.js
+++ Agencia-publicidad/js/validaciones.js
@@ -2,6 +2,7 @@ function validarVacio(valor) {
     return valor.trim() !== "";
 }
 
+
 function validarEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
@@ -11,6 +12,7 @@ function validarLongitud(valor, min, max) {
     return valor.length >= min && valor.length <= max;
 }
 
+//funcion porro
 function validarContrasena(pass) {
     //Una mayúscula, una minúscula, un número y un símbolo
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
@@ -19,6 +21,7 @@ function validarContrasena(pass) {
 
 function validarNif(nif) {
   const regex = /^[A-Z]\d{7}[A-Z0-9]$/;
+  //aqui añadiir para mirar si la letra de control es correcta
   return regex.test(nif);
 }
 
