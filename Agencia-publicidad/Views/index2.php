de8cd2dd06f2a5032b6f48d9e33c84d20e3c742c
David Gonzalez <david.gonzalez.48848@ikasle.egibide.org>
2025-10-20 09:56:44 +0200

    feat:uploaded js for login button

--- Agencia-publicidad/Views/index2.php
+++ Agencia-publicidad/Views/index2.php
@@ -3,24 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pagina principal</title>
     <link rel="stylesheet" href="../css/layout.css"/>
 </head>
 <body>
 
 <header>
-    <img src="../img/logo_SSombra.png" alt="logo" class="logo">
+    <img src="../img/logo_SSombra.png" alt="Logo de comerciantes vitoria" class="logo">
     <div id="buscar">
         <input type="text" class="search"> 
         <button>Buscar</button>
-
-
     </div>
-    <img src="../img/login.png" alt="" class="boton">
-    <img src="../img/lampara.png" alt="lampara" class="lampara">
+    <img src="../img/login.png" alt="Boton de login" class="boton" id="btnLogin">
+    <img src="../img/lampara.png" alt="Boton de cambio de tema" class="lampara">
 </header>
 
-    
 
+<script src="../js/index.js"></script>
 </body>
 </html>
---
+++ Agencia-publicidad/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('btnLogin')?.addEventListener('click', () => {
+    // ejemplo: redirigir a la pantalla de login (ajusta la URL si hace falta)
+    window.location.href = '../index.php?controller=OutController&accion=login';
+});
