f0c759cf56959693fd1eddc54ab2ba9750bfd30c
DaniTo <dani.tomicic@ikasle.egibide.org>
2025-10-20 10:19:44 +0200

    feat:started reajusted view

--- Agencia-publicidad/Views/index2.php
+++ Agencia-publicidad/Views/index2.php
@@ -1,5 +1,9 @@
+<?php
+    //Comprobar session
+    require_once __DIR__ . '/../utils/auth_helper.php';
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,10 +17,19 @@
     <div id="buscar">
         <input type="text" class="search"> 
     </div>
-    <img src="../img/login.png" alt="Boton de login" class="boton" id="btnLogin">
+    <a href="../index.php?controller=OutController&accion=iniciarSesion">
+        <img src="../img/login.png" alt="Boton de login" id="btnLogin">
+    </a>
     <img src="../img/lampara.png" alt="Boton de cambio de tema" class="lampara">
 </header>
 
+<div class="card-anuncio">
+    <div class="div-tj-img">
+        <img src="" alt="">
+        <!-- Imagen que tendrá src autogenerado y alt igual -->
+    </div>
+    <h2 id="titulo-anuncio" ><?php $anuncio['titulo'] ?? 'titulo anuncio generico' ?></h2>
+</div>
 
 <script src="../js/index.js"></script>
 </body>
