b1bbbfcb8ffdd90a3340021db651d7884255cba2
KoldoSoriguren <koldo.soriguren@ikasle.egibide.org>
2025-10-20 09:44:38 +0200

    feat: cresated a temporal view

--- Agencia-publicidad/Views/portada.php
+++ Agencia-publicidad/Views/portada.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Comercio Vitoria</title>
   <link rel="stylesheet" href="../css/layout.css"/>
+  <link rel="stylesheet" href="../css/portada.css">
 </head>
 <body>
     <header></header>
--- Agencia-publicidad/Views/temporal.php
+++ Agencia-publicidad/Views/temporal.php
@@ -8,6 +8,18 @@
 
 </head>
 <body>
+<header>
+    <img src="../img/logo_SSombra.png" alt="logo" class="logo">
+    <div id="buscar">
+        <input type="text" class="search"> 
+        <button>Buscar</button>
+
+
+    </div>
+    <img src="../img/login.png" alt="" class="boton">
+    <img src="../img/lampara.png" alt="lampara" class="lampara">
+</header>
+
     
 </body>
 </html>
