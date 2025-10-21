6ee84bc3ad9b54dc3aba0d1c1889c2c3e858d383
KoldoSoriguren <koldo.soriguren@ikasle.egibide.org>
2025-10-21 10:49:14 +0200

    feat:get drom database the favourite adds

--- Agencia-publicidad/Views/index.php
+++ Agencia-publicidad/Views/index.php
@@ -5,42 +5,74 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina principal</title>
-    <link rel="stylesheet" href="../css/layout.css"/>
-    <link rel="stylesheet" href="../css/index.css"/>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/layout.css"/>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/index.css"/>
 </head>
 <body>
     <header>
-        <img src="../img/logo_SSombra.png" alt="Logo de comerciantes vitoria" class="logo">
+        <img src="<?= BASE_URL ?>/img/logo_SSombra.png" alt="Logo de comerciantes vitoria" class="logo">
         
         <div id="buscar">
-            <input type="text" class="search"> 
+            <div class="search-container">
+                <input type="text" class="search" placeholder="Buscar...">
+                <select name="fruta" id="filtros">
+                    <option value="manzana">Manzana</option>
+                    <option value="banana">Banana</option>
+                    <option value="naranja">Naranja</option>
+                    <option value="kiwi">Kiwi</option>
+                </select>
+            </div>
         </div>
         
         <?php if (isset($isLoggedIn) && $isLoggedIn == false):?>
             <?= $isLoggedIn ?>
-            <a href="../index.php?controller=OutController&accion=iniciarSesion" class="login">
-                <img src="../img/login.png" alt="Boton de login" id="btnLogin">
+            <a href="<?= BASE_URL ?>/index.php?controller=OutController&accion=iniciarSesion" class="login">
+                <img src="<?= BASE_URL ?>/img/login.png" alt="Boton de login" id="btnLogin">
             </a>
         <?php endif; ?>
 
         <?php if (isset($isLoggedIn) && $isLoggedIn == true):?>
-            <p>B1</p>
-            <p>B2</p>
-            <p>B3</p>
-            <?php if ($isAdmin):?>
-                <p>B4</p>    
-            <?php endif; ?>
+            <div id="iconitos">
+                <div class="user-menu-container">
+                    <img src="<?= BASE_URL ?>/img/User.png" alt="persona" id="userMenuToggle" class="user-icon">
+                
+                    <!-- Menú desplegable -->
+                    <div id="userDropdownMenu" class="user-dropdown-menu">
+                        <a href="<?= BASE_URL ?>/index.php?controller=OutController&accion=favourites" class="menu-item">Mis favoritos</a>
+                        <a href="<?= BASE_URL ?>/index.php?controller=OutController&accion=misMensajes" class="menu-item menu-item-notification">
+                            Mis mensajes
+                            <span class="notification-badge"></span>
+                        </a>
+                        <a href="<?= BASE_URL ?>/index.php?controller=OutController&accion=misAnuncios" class="menu-item">Mis anuncios</a>
+                        <a href="<?= BASE_URL ?>/index.php?controller=OutController&accion=miPerfil" class="menu-item">Mi perfil</a>
+                        <a href="<?= BASE_URL ?>/index.php?controller=OutController&accion=logout" class="menu-item menu-item-logout">Cerrar sesión</a>
+                    </div>
+                </div>
+            
+                <img src="<?= BASE_URL ?>/img/Bell.png" alt="campana">
+                <img src="<?= BASE_URL ?>/img/Heart.png" alt="corazon">
+                <?php if ($isAdmin):?>
+                    <a href="<?= BASE_URL ?>/index.php?controller=OutController&accion=store" class="admin-register-btn" title="Registrar nuevo usuario">
+                        <span class="admin-plus">+</span>
+                        <span class="admin-tag">admin</span>
+                    </a>
+                <?php endif; ?>
+
+
+            </div>
+        
+            
         <?php endif; ?>
 
-        <img src="../img/lampara.png" alt="Boton de cambio de tema" class="lampara">
+        <img src="<?= BASE_URL ?>/img/lampara.png" alt="Boton de cambio de tema" class="lampara">
 
     </header>
     <hr>
     <main>
-        
-    <div class="card-anuncio">
+        <div class="cards-row">
+                <div class="card-anuncio">
         <div class="div-tj-img">
-            <img src="../img/logo.png" alt="logo">
+            <img src="<?= BASE_URL ?>/img/logo.png" alt="logo">
             <!-- Imagen que tendrá src autogenerado y alt igual -->
         </div>
         <h2 id="titulo-anuncio" >Titulo 1</h2>
@@ -49,7 +81,7 @@
 
         <div class="card-anuncio">
         <div class="div-tj-img">
-            <img src="../img/logo.png" alt="logo">
+            <img src="<?= BASE_URL ?>/img/logo.png" alt="logo">
             <!-- Imagen que tendrá src autogenerado y alt igual -->
         </div>
         <h2 id="titulo-anuncio" >Titulo 1</h2>
@@ -58,7 +90,7 @@
 
         <div class="card-anuncio">
         <div class="div-tj-img">
-            <img src="../img/logo.png" alt="logo">
+            <img src="<?= BASE_URL ?>/img/logo.png" alt="logo">
             <!-- Imagen que tendrá src autogenerado y alt igual -->
         </div>
         <h2 id="titulo-anuncio" >Titulo 1</h2>
@@ -67,15 +99,18 @@
 
         <div class="card-anuncio">
         <div class="div-tj-img">
-            <img src="../img/logo.png" alt="logo">
+            <img src="<?= BASE_URL ?>/img/logo.png" alt="logo">
             <a href=""></a>
             <!-- Imagen que tendrá src autogenerado y alt igual -->
         </div>
         <h2 id="titulo-anuncio" >Titulo 1</h2>
         <p id="desc-anuncio">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus vero placeat reiciendis necessitatibus facilis reprehenderit nihil sunt omnis amet fuga assumenda, consequatur natus, blanditiis pariatur neque quam quas repellat qui!</p>
     </div>
+        </div>
+                
 
-    <script src="../js/index.js"></script>
+    <script src="<?= BASE_URL ?>/js/index.js"></script>
     </main>
+
 </body>
 </html>
