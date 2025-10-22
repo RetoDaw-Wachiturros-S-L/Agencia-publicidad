3b77ee91edb672a639abbfd345e058064e09336f
KoldoSoriguren <koldo.soriguren@ikasle.egibide.org>
2025-10-22 09:38:53 +0200

    feat: add functions to some icons

--- Agencia-publicidad/controllers/OutController.php
+++ Agencia-publicidad/controllers/OutController.php
@@ -268,6 +268,19 @@ class OutController {
             exit;
         }
     }
+    public function favourites(){
+        $id=$_SESSION["usuario"]["id"];
+        $anuncios = $this->dbUser->sacarfavoritos($id);
+        require_once BASE_URL.'views/favourites.php';
+           
+    }
+    public function verAnuncios(){
+        $id=$_SESSION["usuario"]["id"];
+        $anuncios = $this->dbUser->verMisAnuncios($id);
+        
+
+    }
+
 
 }
 
