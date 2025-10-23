9b1cb56a87e066f8ebafb876bd14e3dd359113f6
David Gonzalez <david.gonzalez.48848@iksale.egibide.org>
2025-10-23 13:48:00 +0200

    feat: added maze analytics tracking script to all views

--- Agencia-publicidad/views/favoritos.php
+++ Agencia-publicidad/views/favoritos.php
@@ -6,6 +6,27 @@
     <title>Mis Favoritos</title>
     <link rel="stylesheet" href="<?= BASE_URL ?>/css/layout.css"/>
     <link rel="stylesheet" href="<?= BASE_URL ?>/css/index.css"/>
+    <script>
+    (function (m, a, z, e) {
+      var s, t;
+      try {
+        t = m.sessionStorage.getItem('maze-us');
+      } catch (err) {}
+
+      if (!t) {
+        t = new Date().getTime();
+        try {
+          m.sessionStorage.setItem('maze-us', t);
+        } catch (err) {}
+      }
+
+      s = a.createElement('script');
+      s.src = z + '?apiKey=' + e;
+      s.async = true;
+      a.getElementsByTagName('head')[0].appendChild(s);
+      m.mazeUniversalSnippetApiKey = e;
+    })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'e76390a3-92da-44e0-9412-672022d0d84d');
+    </script>
 </head>
 <body>
 <main>
