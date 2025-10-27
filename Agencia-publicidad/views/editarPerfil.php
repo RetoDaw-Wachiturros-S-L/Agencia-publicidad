06a1c8948f2be3b2bdf1d9f8877a6ba47e0c684f
KoldoSoriguren <koldo.soriguren@ikasle.egibide.org>
2025-10-27 10:27:19 +0100

    feat: try to implemente edit my profile

--- Agencia-publicidad/views/editarPerfil.php
+++ Agencia-publicidad/views/editarPerfil.php
@@ -17,30 +17,42 @@ use AgenciaPublicidad\Models\TipoPersonaEnum;
     <title>Document</title>
     <link rel="stylesheet" href="../css/editarPerfil.css"/>
 
+
 </head>
 <body>
 
     <main>
         <?php if(empty($_SESSION["usuario"]["foto_perfil"])):?>
             <h2><img src="../img/fotoperfil.png" alt="fotoperfil"> Editar usuario</h2>
-        <?php  else:?>
+        <?php else:?>
             <h2><img src="" alt="fotoperfil"> Editar usuario</h2>
         <?php endif?>
 
         <h1>Campos a cambiar</h1>
-        <div class="botones">
-            <a href="">Nombre</a>
-            <a href="">Apellido</a>
-            <a href="">Contraseña</a>
-            <a href="">Foto de perfil</a>
-            <?php if($_SESSION["usuario"]["tipo"]=="COMERCIANTE"):?>
-                <a href="">Nombre de la empresa</a>
-                <a href="">Comentario de la empresa</a>
-                <a href="">Numero de telefono</a>
-                <a href="">Nif de la empresa</a>
-            <?php endif?>
-        </div>
+            <form class="botones" method="post" action="procesarEdicion.php" enctype="multipart/form-data">
+                <input type="text" placeholder="Nombre" name="nombre"/>
+                <input type="text" placeholder="Apellido" name="apellido"/>
+                <input type="password" placeholder="Contrasña" name="contrasena"/>
+                
+                <!-- Input file personalizado -->
+                <div class="custom-file">
+                    <input type="file" id="foto_perfil" name="foto_perfil"/>
+                    <label for="foto_perfil"> Foto de perfil</label>
+                </div>
+                
+                <?php if($_SESSION["usuario"]["tipo"]=="COMERCIANTE"):?>
+                    <input type="text" placeholder="Nombre de la empresa" name="nombre_empresa"/>
+                    <input type="text" placeholder="Comentario de la empresa" name="comentario_empresa"/>
+                    <input type="text" placeholder="Número de teléfono" name="telefono_empresa"/>
+                    <input type="text" placeholder="NIF de la empresa" name="nif_empresa"/>
+                <?php endif?>
+                
+                <button type="submit" class="boton-enviar">Guardar cambios</button>
+            </form>
+
     </main>
+
+
 
     
 
